ef4722dc81fce233a35daf9d2d9055c485b212d2
Jan Skála <j.skala@hotmail.com>
2024-12-28 19:56:16 +0100

    Load credentials from UI

--- src/Desktop/ViewModels/MainWindowViewModel.cs
+++ src/Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace Desktop.ViewModels;
+﻿using System;
+
+namespace Desktop.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string TidepoolUsername { get; set; } = "";
+    public string TidepoolPassword { get; set; } = "";
+    public string NightscoutUrl  { get; set; } = "";
+    public string NightscoutApiKey  { get; set; } = "";
+
+    public void Test()
+    {
+        Console.WriteLine($"TidepoolUsername: {TidepoolUsername}");
+        Console.WriteLine($"TidepoolPassword: {TidepoolPassword}");
+        Console.WriteLine($"NightscoutUrl: {NightscoutUrl}");
+        Console.WriteLine($"NightscoutApiKey: {NightscoutApiKey}");
+    }
 }
